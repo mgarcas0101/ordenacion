ff9b57163cc044d48995a27550d219e55d113848
mgarcas0101 <mgarcas0101@g.educaand.es>
2025-04-08 10:07:06 +0200

    documentos

--- dos_valores.py
+++ dos_valores.py
@@ -1,10 +1,23 @@
-# Se trata de tomar una lista de dos valores desordenados y producir una salida en orden creciente.
+#Archivo: treselementos.py
+#Fecha: 08/04/2025
+#proy.: estudio de algoritmos 
+#
+# Ordenar crecientemente una lista de tres valores
+# Existe un error, ya que según los valores, puede cambiar
+# dos que entre ellos había que cambiar, perogenera mal orden
+# en los dos posteriores. 
 
-n = [65, 23]
-if n[0] > n[1]:
-    a = n[0]
-    n[0] = n[1]
-    n[1] = a
-    print(n)
-else:
-    print(n)
+n = [252, 14, 58]
+def swap(j):
+    a = n[j]
+    n[j] = n[j+i]
+    n[j+i] = a
+
+
+
+    for i in range(5):
+        for j in range(5):
+            if n[j]>n[j+l]:
+                swap(j)
+
+print(n)
